12871a03af2802116d66f22cfc006c4df14f1a72
akashgaikwad28 <akash.gaikwad@gmail.com>
2025-09-11 17:01:04 +0530

    add data ingestion function in main file

---
+++ main.py
@@ -0,0 +1,14 @@
+from src.Wine_Quality_Prediction import logger
+from src.Wine_Quality_Prediction.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.initiate_data_ingestion()  # <-- Call the method
+
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
