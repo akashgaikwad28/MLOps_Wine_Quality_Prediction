03d4c38e98b16638aa3564afbd2526382a2dc904
akashgaikwad28 <akash.gaikwad@gmail.com>
2025-09-12 21:14:32 +0530

    add data transformation configuration manager

--- src/Wine_Quality_Prediction/config/configuration.py
+++ src/Wine_Quality_Prediction/config/configuration.py
@@ -1,7 +1,7 @@
 from src.Wine_Quality_Prediction.constants import *
 from src.Wine_Quality_Prediction.utils.common import read_yaml, create_directories
 
-from src.Wine_Quality_Prediction.entity.config_entity import (DataIngestionConfig,DataValidationConfig)
+from src.Wine_Quality_Prediction.entity.config_entity import (DataIngestionConfig,DataValidationConfig,DataTransformationConfig)
 
 
 class ConfigurationManager:
@@ -43,3 +43,15 @@ class ConfigurationManager:
         )
 
         return data_validation_config
+    
+    
+    
+    def get_data_transformation_config(self) -> DataTransformationConfig:
+        config=self.config.data_transformation
+        create_directories([config.root_dir])
+        data_transformation_config=DataTransformationConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path
+        )
+        return data_transformation_config
+    
