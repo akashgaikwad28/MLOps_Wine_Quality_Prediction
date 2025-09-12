6a3eede4982f5cf079a05630b2dbe9b3fb1e71a1
akashgaikwad28 <akash.gaikwad@gmail.com>
2025-09-12 21:54:11 +0530

    add model training pipeline functionality in main file

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.Wine_Quality_Prediction import logger
 from src.Wine_Quality_Prediction.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.Wine_Quality_Prediction.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 from src.Wine_Quality_Prediction.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
+from src.Wine_Quality_Prediction.pipeline.model_trainer_pipeline import ModelTrainerTrainingPipeline
 STAGE_NAME = "Data Ingestion stage"
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
@@ -31,6 +32,17 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataTransformationTrainingPipeline()
    data_ingestion.initiate_data_transformation()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Model Trainer stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelTrainerTrainingPipeline()
+   data_ingestion.initiate_model_training()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
